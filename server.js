df414e0b0f29c657f4cc2f0ee363ef988415e9a1
Swapnil <swapnilkant11@gmail.com>
2020-01-29 19:01:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+// This is used to call the express framework for our backend work. */
+const express = require('express');
+
+// This is used to run the server for our backend work.
+const app = express();
+
+// This is used to import static files to the project and to display the content of "client" file here.
+app.use(express.static(__dirname + '/client'));
+
+// Port address
+const port = process.env.port || 3001;
+
+// Function which displays the message that the server has started.
+app.listen(port, function(){
+    console.log('Server running on nodemon successfully on port ' + port);
+});
